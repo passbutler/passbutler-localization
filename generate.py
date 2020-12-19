0e641942b547bc243c3ec0d7df57f977f905a124
Bastian Raschke <bastian.raschke@posteo.de>
2020-12-19 17:07:44 +0100

    Initial commit

---
+++ generate.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+
+import json
+from pathlib import Path
+
+def generateLocalizations():
+    with open('./localizations.json') as localizationsJsonFile:
+        localizationsJsonFileContent = localizationsJsonFile.read()
+        localizationsData = json.loads(localizationsJsonFileContent)
+
+        generateAndroidLocalizations(localizationsData)
+        generateDesktopLocalizations(localizationsData)
+
+def generateAndroidLocalizations(localizationsData):
+    defaultLanguage = localizationsData['default_language']
+
+    localizationTranslationFormatter = lambda localizationTranslationKey, localizationTranslationValue: '<string key="{0}">{1}</string>'.format(localizationTranslationKey, localizationTranslationValue)
+    localizationTables = __generatedLocalizationTables(localizationsData, localizationTranslationFormatter)
+
+    localizationTableOutputBaseDirectory = './output/android/'
+
+    for localizationTableLanguageCode in localizationTables:
+        localizationTableTranslations = localizationTables[localizationTableLanguageCode]
+
+        localizationTableOutputDirectory = localizationTableOutputBaseDirectory + ('values/' if defaultLanguage == localizationTableLanguageCode else 'values-{0}/'.format(localizationTableLanguageCode))
+
+        # Ensures existince of directory
+        Path(localizationTableOutputDirectory).mkdir(parents=True, exist_ok=True)
+
+        localizationTableFileName = '{0}strings.xml'.format(localizationTableOutputDirectory)
+
+        with open(localizationTableFileName, 'w') as localizationTableFile:
+            localizationTableOutput = '\n'.join(localizationTableTranslations)
+            localizationTableFile.writelines(localizationTableOutput)
+
+def generateDesktopLocalizations(localizationsData):
+    localizationTranslationFormatter = lambda localizationTranslationKey, localizationTranslationValue: '{0} = "{1}"'.format(localizationTranslationKey, localizationTranslationValue)
+    localizationTables = __generatedLocalizationTables(localizationsData, localizationTranslationFormatter)
+
+    localizationTableOutputDirectory = './output/desktop/'
+    
+    # Ensures existince of directory
+    Path(localizationTableOutputDirectory).mkdir(parents=True, exist_ok=True)
+
+    for localizationTableLanguageCode in localizationTables:
+        localizationTableTranslations = localizationTables[localizationTableLanguageCode]
+
+        localizationTableFileName = '{0}Messages_{1}.properties'.format(localizationTableOutputDirectory, localizationTableLanguageCode)
+
+        with open(localizationTableFileName, 'w') as localizationTableFile:
+            localizationTableOutput = '\n'.join(localizationTableTranslations)
+            localizationTableFile.writelines(localizationTableOutput)
+
+def __generatedLocalizationTables(localizationsData, localizationTranslationFormatter):
+    localizationSections = localizationsData['sections']
+
+    generatedLocalizationTables = {}
+
+    for localizationSection in localizationSections:
+        sectionName = localizationSection['name']
+        sectionLocalizations = localizationSection['localizations']
+
+        for localizationKey in sectionLocalizations:
+            localizationValues = sectionLocalizations[localizationKey]
+
+            for localizationLanguageCode in localizationValues:
+                localizationTranslationKey = localizationKey
+                localizationTranslationValue = localizationValues[localizationLanguageCode]
+
+                formattedLocalizationTranslation = localizationTranslationFormatter(localizationTranslationKey, localizationTranslationValue)
+                generatedLocalizationTables.setdefault(localizationLanguageCode, []).append(formattedLocalizationTranslation)
+
+    return generatedLocalizationTables
+
+if __name__ == '__main__':
+    print('Generate localization files')
+
+    # TODO: clean output before
+    generateLocalizations()
